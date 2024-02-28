63687a7fc5b4156837f6ef0b45eab099d1cc10f8
Lily McClain <lilymac000@gmail.com>
2024-02-28 18:55:49 -0500

    Added input and quit option

--- calculator.py
+++ calculator.py
@@ -3,5 +3,16 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
+#check first item of the list
+#use that to map to the functions in arithmetic.py
+#call the function on the other list elements 
+
+user_input = input("Enter your equation > ")
+tokens = user_input.split(' ')
+
+if tokens[0] == "q":
+    quit
+else:
+    
 
 # Replace this with your code
