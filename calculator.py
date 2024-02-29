f45e10f94faea970c17425f1c6b2574118b3d40f
Lily McClain <lilymac000@gmail.com>
2024-02-28 19:22:12 -0500

    Fixed input issues and added loop

--- calculator.py
+++ calculator.py
@@ -7,27 +7,35 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 #use that to map to the functions in arithmetic.py
 #call the function on the other list elements 
 
-user_input = input("Enter your equation > ")
-tokens = user_input.split(' ')
+while True:
+    user_input = input("Enter your equation > ")
+    temp = user_input.split(' ')
 
-if tokens[0] == "q":
-    quit
-else:
-    if tokens[0] == "+":
-        print(add(tokens[1],tokens[2]))
-    elif tokens[0] == "-":
-        print(subtract(tokens[1],tokens[2]))
-    elif tokens[0] == "*":
-        print(multiply(tokens[1],tokens[2]))
-    elif tokens[0] == "/":
-        print(divide(tokens[1],tokens[2]))
-    elif tokens[0] == "square":
-        print(square(tokens[1]))
-    elif tokens[0] == "cube":
-        print(cube(tokens[1]))
-    elif tokens[0] == "pow":
-        print(power(tokens[1],tokens[2]))
-    elif tokens[0] == "mod":
-        print(mod(tokens[1],tokens[2]))
+    if len(temp) == 3:
+        tokens = [temp[0], int(temp[1]), int(temp[2])]
+    elif len(temp) == 2:
+        tokens = [temp[0], int(temp[1])]
+    elif len(temp) == 1:
+        tokens = temp[0]
+
+    if tokens[0] == "q":
+        break
+    else:
+        if tokens[0] == "+":
+            print(add(tokens[1],tokens[2]))
+        elif tokens[0] == "-":
+            print(subtract(tokens[1],tokens[2]))
+        elif tokens[0] == "*":
+            print(multiply(tokens[1],tokens[2]))
+        elif tokens[0] == "/":
+            print(divide(tokens[1],tokens[2]))
+        elif tokens[0] == "square":
+            print(square(tokens[1]))
+        elif tokens[0] == "cube":
+            print(cube(tokens[1]))
+        elif tokens[0] == "pow":
+            print(power(tokens[1],tokens[2]))
+        elif tokens[0] == "mod":
+            print(mod(tokens[1],tokens[2]))
 
 # Replace this with your code
